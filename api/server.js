c16e9cc1cdc2b3fed0716787a780dbb7b15915bc
zxh <zhangxiuhong@zyxtravel.com>
2019-03-19 10:46:52 +0800

    register routes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var routes = require('./routes/client'); //importing route
+routes(app); //register the route
+
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
