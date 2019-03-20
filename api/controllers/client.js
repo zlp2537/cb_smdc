d98f90a56bec32f1c66812026225277ac8db38e6
xiuhong.zhang <xiuhong.zhang@bitmain.com>
2019-03-20 08:15:18 +0800

    code refactor

--- api/controllers/client.js
+++ api/controllers/client.js
@@ -6,27 +6,26 @@ const Menu = require('../models/menu');
 exports.menu = function(req, res) {
   const rId = req.params.restaurantId;
   const tId = req.params.tableId;
-  console.log(rId, tId);
-
-  Restaurant.findOne({id: rId}, function(err, r) {
-    console.log(err, r);
-    if (r == null) {
+  //console.log(rId, tId);
+  loadMenu(rId, function(err, menu) {
+    if (err || menu == null) {
       res.status(404).send('Sorry, we cannot find that!'); // TODO
-      return;
-    };
-
-    const mId = r.menu_id;
-    console.log(mId);
-    Menu.findOne({id: mId}, function(err, menu) {
-      if (r == null) {
-        res.status(404).send('Sorry, we cannot find that!'); // TODO
-        return;
-      };
-
+    } else {
       res.json(menu);
-    });
+    }
   });
 };
+
+function loadMenu(restaurantId, cb) {
+  Restaurant.findOne({id: restaurantId}, function(err, r) {
+    if (err || r == null) {
+      return cb(err, r);
+    }
+    Menu.findOne({id: r.menu_id}, function(err, menu) {
+      cb(err, menu);
+    });
+  });
+}
 
 // exports.create_a_task = function(req, res) {
 //   var new_task = new Task(req.body);
