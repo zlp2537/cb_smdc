4f2f5d45b55f5eb7b2d36aaf6c72687d5e44d9ae
xiuhong.zhang <xiuhong.zhang@bitmain.com>
2019-03-20 12:16:14 +0800

    Update menu.js

--- api/models/menu.js
+++ api/models/menu.js
@@ -2,7 +2,17 @@
 
 const mongoose = require('mongoose');  
 const menuSchema = new mongoose.Schema({  
-  id: String
+  id: String,
+  groups: [{
+    id: Number,
+    name: String
+  }],
+  items: [{
+    group_id: Number,
+    name: String,
+    pic_id: String,
+    price: Number // in cents
+  }]
 });
 mongoose.model('Menu', menuSchema);
 
